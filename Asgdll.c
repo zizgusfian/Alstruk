84e290ac515de9b1e8324f1fed2d5e33c09b0857
zizgusfian <62132245+zizgusfian@users.noreply.github.com>
2020-06-04 08:57:22 +0700

    Create Asgdll.c

---
+++ Asgdll.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{ /* Kamus */
+float f;
+long double fll;
+/* Algoritma */
+f= 20.0f;
+fll=10.0L;
+
+printf("float f = %f \n", f);
+printf("long double fll = %fl \n", fll);
+return 0;
+}
