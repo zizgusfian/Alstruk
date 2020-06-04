4e6de9d0502bdc8ac2e29160cb249b75cfde1822
zizgusfian <62132245+zizgusfian@users.noreply.github.com>
2020-06-04 08:55:57 +0700

    Create Asign.c

---
+++ Asign.c
@@ -0,0 +1,14 @@
+/* Assignment nilai integer dan print */
+#include <stdio.h>
+
+int main ()
+{
+/* Kamus */
+int i;
+
+/* Program */
+printf ("hello\n");
+i = 5;
+printf ("Ini nilai i : %d \n", i);
+return 0;
+}
