a69f17110e76566c2ac387c08644e3143bff9d29
zizgusfian <62132245+zizgusfian@users.noreply.github.com>
2020-06-04 08:47:02 +0700

    Create Hello.c

---
+++ Hello.c
@@ -0,0 +1,11 @@
+/* menuliskan hello ke layar */
+/* pola ini merupakan standard yang dipakai di kelas */
+#include <stdio.h>
+
+int main ()
+{
+/* KAMUS */
+/* ALGORITMA */
+    printf ("hello\n");
+return 0;
+}
